49d2c0e1c1654da1ee059c0b51dfd06a6c4b4a5f
Thulio <thulio.alves@gmail.com>
2020-07-31 01:56:56 -0300

    Refatoração e legibilidade do código

--- scrape.py
+++ scrape.py
@@ -2,20 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar/'
+
+print("\nAcessando site da Agência Nacional de Saúde Suplementar: ", url)
 response = requests.get(url)
 page = response.content
 soup = BeautifulSoup(page, 'html.parser')
 
-ans = soup.find('a', target='_self').attrs['href']
-r1 = requests.get('http://www.ans.gov.br' + ans)
-url_especifica = 'http://www.ans.gov.br' + ans
+ANS = soup.find('a', target='_self').attrs['href']
+url_especifica = 'http://www.ans.gov.br' + ANS
+print("\nAcessando url da versão atualizado:", url_especifica)
 
 response_especifica = requests.get(url_especifica)
 page_especifica = response_especifica.content
 soup = BeautifulSoup(page_especifica, 'html.parser')
 
-divPdf = soup.find('div', class_='table-responsive')
-aPdfHref = divPdf.find('a', target='_self').attrs['href']
+print("Procurando arquivo em PDF do Padrão para Troca de Informação de Saúde Suplementar - (TISS)\n")
+aPdfHref = soup.find('a', target='_self').attrs['href']
 r = requests.get('http://www.ans.gov.br' + aPdfHref)
 file = aPdfHref[aPdfHref.rfind('/') + 1:]
-open(file, 'wb').write(r.content)
+
+if file.endswith('.pdf'):
+    print("Arquivo encontrado:", file, "e feito download.")
+    open(file, 'wb').write(r.content)
+else:
+    print("Não encontramos o arquivo solicitado")
