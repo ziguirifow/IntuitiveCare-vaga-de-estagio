07b66582a68d2210c9e6a46f521a1be0ba67913d
Thulio <thulio.alves@gmail.com>
2020-08-01 14:23:05 -0300

    Melhorando legibilidade dos outputs de texto

--- scrape.py
+++ scrape.py
@@ -1,17 +1,20 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
-import os
 
 url = 'http://www.ans.gov.br/prestadores/tiss-troca-de-informacao-de-saude-suplementar/'
 
-print("\nAcessando site da Agência Nacional de Saúde Suplementar: ", url)
+print("\nAcessando site da Agência Nacional de Saúde Suplementar:")
+print(url)
 response = requests.get(url)
 page = response.content
 soup = BeautifulSoup(page, 'html.parser')
 
 ANS = soup.find('a', target='_self').attrs['href']
 url_especifica = 'http://www.ans.gov.br' + ANS
-print("\nAcessando url da versão atualizado:", url_especifica)
+print("\nAcessando link da versão atualizado:")
+print(url_especifica)
 
 response_especifica = requests.get(url_especifica)
 page_especifica = response_especifica.content
@@ -26,6 +29,6 @@ if file.endswith('.pdf'):
     with open(fr'\Users\{os.getlogin()}\Desktop\{file}', 'wb') as file:
         file.write(r.content)
         file.close()
-    print("Arquivo encontrado e salvo no caminho:", file.name)
+    print("Arquivo encontrado e salvo em:", file.name, "\n")
 else:
     print("Não encontramos o arquivo solicitado")
